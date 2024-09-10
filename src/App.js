13b47c071a0e44302ddc0b3f87e2cbb046a1831a
Anderson Lucas <114498251+andersoncsgo@users.noreply.github.com>
2024-09-10 11:15:20 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [dice1, setDice1] = useState(1);
+  const [dice2, setDice2] = useState(1);
+  const [message, setMessage] = useState("");
+  const [messageColor, setMessageColor] = useState("white");
+  const [wins, setWins] = useState(0);
+  const [totalGames, setTotalGames] = useState(0);
+
+
+  const rollDice = () => {
+    const newDice1 = Math.floor(Math.random() * 6) + 1;
+    const newDice2 = Math.floor(Math.random() * 6) + 1;
+    const sum = newDice1 + newDice2;
+
+    setDice1(newDice1);
+    setDice2(newDice2);
+    setTotalGames(totalGames + 1);
+
+    if (sum === 7 || sum === 11) {
+      setMessage("Você ganhou no Tigrinho do dado🤑");
+      setMessageColor("green"); 
+      setWins(wins + 1);
+    } else {
+      setMessage("Você perdeu no Tigrinho do dado😰");
+      setMessageColor("red"); 
+    }
+  };
+
+  
+  const backgroundStyle = {
+    backgroundImage: `url("https://images.prismic.io/assert2/9b4b36bd-d165-4426-9d5c-1b9fd403a4dd_capa-jogo-do-tigrinho-pol%C3%AAmica-influenciadores+.png?auto=format,compress")`,
+    backgroundPosition: "center",
+    display: "flex",
+    justifyContent: "center",
+  };
+
   return (
-    <div className="App">
+    <div style={backgroundStyle}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Jogo de Dados</h1>
+        <div className="dice-container">
+          <div className="dice">{dice1}</div>
+          <div className="dice">{dice2}</div>
+        </div>
+        <button onClick={rollDice}>Jogar</button>
+        <p style={{ color: messageColor }}>{message}</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Score: {wins}/{totalGames} ={" "}
+          {totalGames > 0 ? ((wins / totalGames) * 100).toFixed(2) : 0}%
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
